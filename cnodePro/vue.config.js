a3407a1f78b20ed7ff5c467c716fd66aa3240e83
zhou <410983459@qq.com>
2020-09-23 16:11:23 +0800

    打包信息修改

--- cnodePro/vue.config.js
+++ cnodePro/vue.config.js
@@ -7,7 +7,11 @@ function resolve (dir) {
 
 // vue.config.js
 module.exports = {
-  publicPath: '/',
+  // 基本路径
+  publicPath:"./",  // 可以设置成相对路径，这样所有的资源都会被链接为相对路径，打出来的包可以被部署在任意路径
+  // outputDir:"dist",  //打包时生成的生产环境构建文件的目录
+  // assetsDir: 'public',  // 放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录
+
   /*
     Vue-cli3:
     Crashed when using Webpack `import()` #2463
